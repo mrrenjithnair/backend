a5ab33a1f12c1e5321963bcc1e38026809cf6f71
Renjith <renjith.gencue@outlook.com>
2022-02-22 02:26:40 -0800

    removed unwanted code

--- api-service/api/controllers/securityController.js
+++ api-service/api/controllers/securityController.js
@@ -4,10 +4,7 @@ const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const controllerUtils = require('../../controllerUtils.js');
 const db = require('../../db/dbConnection.js'); //required for applying transaction
 const securityDao = require('./../../dao/securityDao.js');
-const httpUtils = require('./../../httpUtils.js');
-
 const config = require('../../config.js').getSecurityConfig();
-
 const TOKEN_EXPIRATION_TIME = config.security.expires;
 
 module.exports = {
@@ -15,13 +12,11 @@ module.exports = {
 };
 
 function authenticate(req, res) {
-    // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
     let user = req.swagger.params.authRequest.value;
     let webapp = user.webapp ? user.webapp : false
     let authenticateUser = () => {
         return securityDao.authenticate(user.username, user.password, req.headers.secret, req.headers.deviceid, webapp )
         .then((userModel) => {
-            //Keep only the necessary items in token object
             if (userModel && userModel.length>0) {
                 var user = {
                     id: userModel[0].id,
@@ -31,12 +26,10 @@ function authenticate(req, res) {
                     emailId: userModel[0].emailId,
                 }
             }
-            // if user is found and password is right, create a token
             let token = jwt.sign(user, config.security.secret, {
                 expiresIn: TOKEN_EXPIRATION_TIME
             })
             console.log('token',token)
-
             return {
                 token: token,
                 expires: TOKEN_EXPIRATION_TIME,
--- api-service/dao/securityDao.js
+++ api-service/dao/securityDao.js
@@ -16,25 +16,6 @@ const modelUtils = require('./../modelUtils.js');
 const AUTHENTICATE_QUERY = " SELECT * FROM USER U " +
     " where (u.username = :username or u.emailId = :username) and u.deletedAt is null ";
 
-const UPDATE_DEVICE_USER = " UPDATE DEVICE_TOKEN SET USERID = :providerId WHERE deviceId = :deviceId  ";
-
-const UPDATE_LAST_LOGIN = " UPDATE USER SET LASTLOGIN =:lastLogin WHERE ID = :userId  ";
-
-const SELECT_PASSWORD_FOR_ADMIN_OPERATOR = "SELECT PASSWORD FROM USER WHERE ROLEID = 1";
-const UPDATE_OTP = "UPDATE USER SET OTP = :password WHERE USERNAME = :username";
-
-const GET_USER_CONTACTS = " SELECT CT.emailId, CT.emailVerified, CT.firstName, CT.lastName, CT.ID contactId, UCT.primaryEmail, UCT.primaryPhone, " +
-    " IF(CT.phoneNumberExtn IS NOT NULL, CONCAT(CT.phoneNumber, ' Extn ', CT.phoneNumberExtn), CT.phoneNumber) phoneNumber " +
-    " FROM USER_CONTACT UCT " +
-    " INNER JOIN CONTACT CT ON CT.ID = UCT.CONTACTID " +
-    " WHERE CT.DELETEDAT IS NULL AND UCT.USERID = :userId "
-
-const GET_USER_TOKEN_AND_VISIT_COUNT = " SELECT U.ID, DT.DEVICETOKEN, COUNT(PVT.VISITID) TOTALVISITCOUNT, CT.ID BACKOFFICESUPPORTID  FROM USER U  " +
-    " LEFT OUTER JOIN DEVICE_TOKEN DT ON DT.USERID = U.ID " +
-    " LEFT OUTER JOIN PROVIDER_VISIT PVT ON PVT.PROVIDERUSERID = U.ID " +
-    " LEFT OUTER JOIN VISIT VI ON VI.ID = PVT.VISITID " +
-    " LEFT OUTER JOIN CHAT_TOPIC CT ON CT.PROVIDERUSERID = U.ID AND CT.EPISODEID IS NULL AND CT.TYPE IS NULL " +
-    " WHERE U.ID = :userId AND VI.DELETEDAT IS NULL " ;
 
 const securityDao = new function() {
     this.authenticate = function(username, password, secret = null, deviceId = null, webapp = false) {
@@ -46,11 +27,8 @@ const securityDao = new function() {
             replacements: queryReplacement,
             type: db.QueryTypes.SELECT
         }).then((r) => {
-            console.log('r=====>',r)
-            // console.log(modelUtils.encrypt('renjithNair'))
             if (r.length != 1)
                 throw exceptionUtil.createSPException(config.security.invalidUserNamePasswd);
-
             try {
                 let passwordFromDB = r[0].password //modelUtils.decrypt(r[0].password);
                 console.log("passwordFromDB",passwordFromDB)
@@ -64,59 +42,10 @@ const securityDao = new function() {
                 throw exceptionUtil.createSPException(config.security.invalidUserNamePasswd);
             }
         }).then((user) => {
-            console.log("user",user)
             return user
         })
     }
 
-    this.getUser = function(username) {
-        let queryReplacement = {
-            'username': username
-        };
-        return db.query(AUTHENTICATE_QUERY, {
-            replacements: queryReplacement,
-            type: db.QueryTypes.SELECT
-        }).then((r) => {
-            if (r.length != 1)
-                throw exceptionUtil.createSPException(config.security.invalidTokenUser);
-
-            let pinFromDB = modelUtil.decryptBlob(r[0].pin);
-            r[0].pin = pinFromDB
-            delete r[0].password;
-            return r;
-        });
-    }
-    this.updateLastLogin =function(userId,lastLogin) {
-        let queryReplacement = {
-            'userId': userId,
-            'lastLogin':lastLogin
-        };
-        return db.query(UPDATE_LAST_LOGIN, {
-            replacements: queryReplacement,
-            type: db.QueryTypes.UPDATE
-        }).then((res) => {
-            return res;
-        });
-    }
-
-    this.fetchUserKey = function(username) {
-        let password = passwordGen.generate({
-            length: sysConfig.OTP.password.length,
-            numbers: sysConfig.OTP.password.includeNumbers,
-            uppercase: sysConfig.OTP.password.includeUpperCase
-        });
-
-        let queryReplacement = {
-            'username': username,
-            'password': password,
-        };
-        return db.query(UPDATE_OTP, {
-            replacements: queryReplacement,
-            type: db.QueryTypes.UPDATE
-        }).then(() => {
-            return {key: password}
-        });
-    }
 };
 
 module.exports = securityDao;
